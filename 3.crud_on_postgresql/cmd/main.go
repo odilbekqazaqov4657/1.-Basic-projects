07a5d367058a24b800ad57689cc7cfc815050471
Odilbek <odilbek.go.dev@gmail.com>
2024-11-17 14:12:42 +0500

    connecting to database

--- 3.crud_on_postgresql/cmd/main.go
+++ 3.crud_on_postgresql/cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+)
+
+var ctx context.Context = context.Background()
 
 func main() {
-	fmt.Println("hello world")
+	Conn()
+}
+
+func Conn() {
+	// psql-U postgres -h localhost -d postgres
+	conn, err := pgx.Connect(ctx, "postgres://postgres:postgres@0.0.0.0:5432/postgres")
+	if err != nil {
+		log.Println("error on connecting to database", err)
+		return
+	}
+	fmt.Println(conn)
 }
